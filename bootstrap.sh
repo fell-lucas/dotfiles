b1f2bed9cc035d08d9beda606629c054ea7ff6ed
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-11-22 10:59:49 -0300

    removed submodule, using package manager for dotdrop and updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
-# git clone --recurse-submodules git@github.com:fell-lucas/dotfiles.git
 
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
-touch ~/.z
 zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 zsh -c "git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1"
 zsh -c "ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
-
-python -m ensurepip --upgrade
-pip3 install -r dotdrop/requirements.txt --user
-
-dotdrop/bootstrap.sh
-./dotdrop.sh install -p "fell-pc" -f
