9fb4b3161a0756aa17d317170e3c95913998130b
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2022-03-26 19:32:54 -0300

    updates zshrc, removes awesome, betterdiscord and spicetify

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
 zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-zsh -c "git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1"
-zsh -c "ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
+zsh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+chsh -s "/usr/bin/zsh" 
+exec zsh
